a09c5274803158f6432e32d97462f345d5e2aadd
postscript100@gmail.com <postscript100@gmail.com>
2020-07-21 15:56:23 +0600

    проба

---
+++ chatbottg/chatbottg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chatbottg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+        }
+    }
+}
